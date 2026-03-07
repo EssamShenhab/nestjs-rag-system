6278691a96e9ec527f2640e02477b9fefeffa9ca
EssamShenhab <essam119999@gmail.com>
2026-03-07 09:32:05 +0200

    refactor: fix module dependency wiring - register modules in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,23 +2,45 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentsModule } from './documents/documents.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/app.config';
 import { validationSchema } from './config/validation';
 import { ProcessModule } from './process/process.module';
 import { ProjectModule } from './project/project.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChunkModule } from './chunk/chunk.module';
+import { AssetModule } from './asset/asset.module';
+import { VectordbModule } from './stores/vectordb/vectordb.module';
 
 @Module({
   imports: [
-    DocumentsModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
       validationSchema,
       load: [configuration],
     }),
-    ProcessModule,
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('db.host'),
+        port: config.get<number>('db.port'),
+        username: config.get<string>('db.username'),
+        password: config.get<string>('db.password'),
+        database: config.get<string>('db.database'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
     ProjectModule,
+    DocumentsModule,
+    ProcessModule,
+    ChunkModule,
+    AssetModule,
+    VectordbModule,
   ],
   controllers: [AppController],
   providers: [AppService],
